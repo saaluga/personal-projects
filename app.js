25243d0761e50318aacd353450ed0f738bd29758
Ambayo Saviour Luga <ambayosaviourluga@gmail.com>
2022-09-12 13:02:16 +0300

    Worked on passport.js and auth.js

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const passport = require('passport');
+const session = require('express-session');
 const connectDB = require('./config/db');
 
 //Load Config Files
 dotenv.config({ path: './config/config.env' });
+
+//Passport Config
+require('./config/passport')(passport);
 
 connectDB();
 
@@ -19,8 +25,25 @@ if (process.env.NODE_ENV === 'development') {
 app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
 
+//Session
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+//Passsport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 //Routes
 app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 3000;
 
