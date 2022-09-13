0504ec1ba5dd07edee3b59087a80cb553cda6486
Ambayo Saviour Luga <ambayosaviourluga@gmail.com>
2022-09-13 16:08:11 +0300

    Worked on _header.hbs

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
 
 //@desc Google auth callback
-//route GET /auth/google/callback
+//@route GET /auth/google/callback
 router.get(
   '/google/callback',
   passport.authenticate('google', { failureRedirect: '/' }),
@@ -15,5 +15,12 @@ router.get(
     res.redirect('/dashboard');
   }
 );
+
+//@desc Logout User
+//@route /auth/logout
+router.get('/logout', (req, res) => {
+  req.logOut();
+  res.redirect('/');
+});
 
 module.exports = router;
