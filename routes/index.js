9d8e09e001b77f790b03c480c80513aa3c4f51d5
Ambayo Saviour Luga <ambayosaviourluga@gmail.com>
2022-09-24 11:25:17 +0300

    Worked on _add_btn.hbs file and rendered it on main.hbs

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { ensureAuth, ensureGuest } = require('../middleware/auth');
 
+const Story = require('../models/Story');
+
 //@desc Login/Landing Page
 //@route GET /
 router.get('/', ensureGuest, (req, res) => {
@@ -12,8 +14,17 @@ router.get('/', ensureGuest, (req, res) => {
 
 //@desc Dashboard
 //route GET /dashboard
-router.get('/dashboard', ensureAuth, (req, res) => {
-  res.render('dashboard', { name: req.user.firstName });
+router.get('/dashboard', ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ user: req.user.id }).lean();
+    res.render('dashboard', {
+      name: req.user.firstName,
+      stories,
+    });
+  } catch (err) {
+    console.log(err);
+    res.render('errors/500');
+  }
 });
 
 module.exports = router;
