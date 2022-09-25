0571e2e22e1039eb6826c2c5e53a9164884de4cb
Ambayo Saviour Luga <ambayosaviourluga@gmail.com>
2022-09-25 10:42:52 +0300

    Implemented Moment to format date in /helpers/hbs.js

--- app.js
+++ app.js
@@ -27,8 +27,18 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+//Handlebar Helpers
+const { formatDate } = require('./helpers/hbs');
+
 //Handlebars
-app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }));
+app.engine(
+  '.hbs',
+  exphbs.engine({
+    helpers: { formatDate },
+    defaultLayout: 'main',
+    extname: '.hbs',
+  })
+);
 app.set('view engine', '.hbs');
 
 //Session
